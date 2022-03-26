f3215ebc8e37bf074718d260bbc6fa4d6d416bac
sufuwang <1137120948@qq.com>
2022-03-26 10:22:52 +0800

    feat: debug login

--- src/utils/entryTask.ts
+++ src/utils/entryTask.ts
@@ -6,6 +6,9 @@ const { SYSTEM_URL } = env;
 console.info('SYSTEM_URL: ', SYSTEM_URL);
 
 const auth = async () => {
+  if (window.location.pathname === 'debug') {
+    return;
+  }
   const sessionId = cookies.get('sessionId');
   if (!sessionId) {
     window.location.href = `${SYSTEM_URL}/login?redirect=${window.location.href}`;
